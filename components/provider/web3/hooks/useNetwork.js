9e6cbd7ad701d29f2647275b4bd41c4e541faeeb
PelumiDev <Pelumiogundipe905@gmail.com>
2022-08-13 00:42:56 +0100

    added some stuff

--- components/provider/web3/hooks/useNetwork.js
+++ components/provider/web3/hooks/useNetwork.js
@@ -1,20 +1,30 @@
 import React from "react"
 import useSWR from "swr"
 
+const NETWORK = {
+    1: "Ethereum Main Network",
+    3: "Ropsen Test Network",
+    4: "Rinkeby Test Network",
+    5: "Goerli Test Network",
+    42: "Kovan Test Network",
+    56: "Binance Smart Chain",
+    1337: "Ganache",
+}
+
 
 export const Handler  = (web3) => () => {
 
     const {mutate, ...rest}  = useSWR(() => 
     web3 ? "web3/network" : null,
     async () => {
-        const netId = await web3.eth.net.getId()
-        return netId
+        const netId = await web3.eth.getChainId()
+        return NETWORK[netId]
     }
     )
     React.useEffect(() => {
         ethereum && 
-        ethereum.on("chainChanged", (netId) => {
-            mutate(netId)
+        ethereum.on("chainChanged", (chainId) => {
+            mutate(parseInt(chainId, 16))
         })
     }, [])
 
